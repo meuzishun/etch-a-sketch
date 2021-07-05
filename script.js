1239064df996a84e806670cb981aec853829c5b6
Andrew Smith <meuzishun@gmail.com>
2021-07-05 13:05:07 -0400

    Create the function for creating a grid

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container');
+
+function buildGrid(num) {
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = 0; j < num; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+        
+        container.appendChild(row);
+    }
+}
+
+buildGrid(16);

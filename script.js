f7b375bb4cedc39d51c6928c1544c50728ed1334
Andrew Smith <meuzishun@gmail.com>
2021-07-06 14:06:07 -0400

    Add darken feature; break up functions

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
 const container = document.querySelector('.container');
 
-const clearBtn = document.querySelector('.clear');
+const clearBtn = document.querySelector('.clear-btn');
 
 clearBtn.addEventListener('click', resetGrid);
 
 function resetGrid() {
     deleteGrid();
-    const size = prompt('How many squares per side would you like the grid to be?');
+    let size = prompt('How many squares per side would you like the grid to be?');
+    if (size === null || size === undefined) return;
+    while (size < 5 || size > 100) {
+        size = prompt('How many squares per side would you like the grid to be? (choose between 5 and 100)');
+        if (size === null || size === undefined) break;
+    }
     buildGrid(size);
+}
+
+function randomNum(num) {
+    return Math.floor(Math.random() * num);
+}
+
+function randomColor() {
+    return `rgb(${randomNum(255)}, ${randomNum(255)}, ${randomNum(255)})`;
+}
+
+function extractNumbers(str) {
+    const rgbNumbers = str.replace('rgb', '').replace('(', '').replace(')','').split(', ').map(num => Number(num));
+    return rgbNumbers;
+}
+
+function darkenColor(rgb) {
+    const newRGB = rgb.map(num => {
+        if (num === 0) return num;
+        if (num <= 25) return 0;
+        if (num > 25) return num - 25;
+    });
+    return newRGB;
+}
+
+function formatColor(arr) {
+    return `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
 }
 
 function addHoveredStyle(evt) {
     const cell = evt.target;
-    cell.style.backgroundColor = 'grey';
+    if (!cell.style.backgroundColor) {
+        cell.style.backgroundColor = randomColor();
+        return;
+    }
+    
+    const rgbNumbers = extractNumbers(cell.style.backgroundColor);
+    const darkerRGB = darkenColor(rgbNumbers);
+    const color = formatColor(darkerRGB);
+    cell.style.backgroundColor = color;
 }
 
 function buildGrid(num) {
@@ -32,7 +71,7 @@ function buildGrid(num) {
 }
 
 function deleteGrid() {
-    const rows = [...container.children];
+    const rows = [...container.querySelectorAll('.row')];
     rows.forEach(row => {
         container.removeChild(row);
     });
